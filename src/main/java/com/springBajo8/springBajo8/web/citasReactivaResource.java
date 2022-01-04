ba565d4df904bdc436051a0353d34b47b2c1abf0
Jpruso4 <74436239+Jpruso4@users.noreply.github.com>
2022-01-03 22:20:46 -0500

    Agregando notacion al controller

--- src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
+++ src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
@@ -24,11 +24,13 @@ public class citasReactivaResource {
 
     @PostMapping("/citasReactivas")
     @ResponseStatus(HttpStatus.CREATED)
+    @CrossOrigin
     private Mono<citasDTOReactiva> save(@RequestBody citasDTOReactiva citasDTOReactiva) {
         return icitasReactivaService.save(citasDTOReactiva);
     }
 
     @DeleteMapping("/citasReactivas/{id}")
+    @CrossOrigin
     private Mono<ResponseEntity<citasDTOReactiva>> delete(@PathVariable("id") String id) {
         return icitasReactivaService.delete(id)
                 .flatMap(citasDTOReactiva -> Mono.just(ResponseEntity.ok(citasDTOReactiva)))
@@ -36,6 +38,7 @@ public class citasReactivaResource {
     }
 
     @PutMapping("/citasReactivas/{id}")
+    @CrossOrigin
     private Mono<ResponseEntity<citasDTOReactiva>> update(@PathVariable("id") String id, @RequestBody citasDTOReactiva citasDTOReactiva) {
         return icitasReactivaService.update(id, citasDTOReactiva)
                 .flatMap(citasDTOReactiva1 -> Mono.just(ResponseEntity.ok(citasDTOReactiva1)))
@@ -43,31 +46,37 @@ public class citasReactivaResource {
     }
 
     @GetMapping("/citasReactivas/{idPaciente}/byidPaciente")
+    @CrossOrigin
     private Flux<citasDTOReactiva> findAllByidPaciente(@PathVariable("idPaciente") String idPaciente) {
         return icitasReactivaService.findByIdPaciente(idPaciente);
     }
 
     @GetMapping(value = "/citasReactivas")
+    @CrossOrigin
     private Flux<citasDTOReactiva> findAll() {
         return this.icitasReactivaService.findAll();
     }
 
     @PutMapping("/citasReactivas/cancelAppointment/{id}")
+    @CrossOrigin
     private Mono<ResponseEntity<citasDTOReactiva>> cancelAppointment(@PathVariable("id") String id) {
         return icitasReactivaService.cancelAppointment(id);
     }
 
     @GetMapping("/citasReactivas/{fechaReservaCita}/{horaReservaCita}")
+    @CrossOrigin
     private Mono<citasDTOReactiva> findByDateAndHour(@PathVariable(value = "fechaReservaCita") String fechaReservaCita, @PathVariable(value = "horaReservaCita") String horaReservaCita) {
         return icitasReactivaService.findByFechaReservaCitaAndHoraReservaCita(LocalDate.parse(fechaReservaCita), horaReservaCita);
     }
 
     @GetMapping("/citasReactivas/doctorName/{id}")
+    @CrossOrigin
     private Mono<citasDTOReactiva> getDoctorName(@PathVariable("id") String id) {
         return icitasReactivaService.getDoctorName(id);
     }
 
     @GetMapping("/citasReactivas/padecimientos/{id}")
+    @CrossOrigin
     private Flux<citasDTOReactiva> getPadecimientos(@PathVariable("id") String idPaciente){
         return icitasReactivaService.getPadecimientos(idPaciente);
     }
